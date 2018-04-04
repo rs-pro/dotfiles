cb136cbebbde42672896a837a031b3fe99146a45
Gleb <glebtv@gmail.com>
2018-04-05 00:45:10 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
-set -x EDITOR='nvim'
-set -x GEM_HOME=$HOME/.gem/ruby/2.5.0
-set -x GEM_PATH=$HOME/.gem/ruby/2.5.0
+set -x EDITOR 'nvim'
+set -x GEM_HOME $HOME/.gem/ruby/2.5.0
+set -x GEM_PATH $HOME/.gem/ruby/2.5.0
 
-set -x GOPATH=/data/go
-set -x GOBIN=$GOPATH/bin
+set -x GOPATH /data/go
+set -x GOBIN $GOPATH/bin
 
-set -x PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin:/data/go/bin
+set -x PATH $PATH:$HOME/.gem/ruby/2.5.0/bin:/data/go/bin
