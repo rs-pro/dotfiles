06381e88df74ebdd26079c570393da9af0d28ace
Gleb <glebtv@gmail.com>
2018-04-05 00:46:12 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,5 @@ set -x GEM_PATH $HOME/.gem/ruby/2.5.0
 set -x GOPATH /data/go
 set -x GOBIN $GOPATH/bin
 
-set -x PATH $PATH:$HOME/.gem/ruby/2.5.0/bin:/data/go/bin
+set -x PATH $PATH $HOME/.gem/ruby/2.5.0/bin
+set -x PATH $PATH /data/go/bin
